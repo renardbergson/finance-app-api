68fe3dda3078c62997bf17941a33bf5a71c6711b
renardbergson <renard.contato@gmail.com>
2025-09-10 08:15:20 -0300

    chore: adding postgres and pg setup

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const person = 'Renard';
-console.log(person);
+import 'dotenv/config.js';
+
+import express from 'express';
+import { postgresHelper } from './src/db/postgres/client.js';
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  const results = await postgresHelper.query('SELECT * FROM users;');
+  res.status(200).send(results);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ src/db/postgres/client.js
@@ -0,0 +1,25 @@
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+});
+
+export const postgresHelper = {
+  query: async (query, params) => {
+    const client = await pool.connect();
+    try {
+      const results = await client.query(query, params);
+      return results.rows;
+    } catch (error) {
+      console.error('Some error has occurred trying to execute query:', error);
+      throw error;
+    } finally {
+      client.release(); // libera o client
+    }
+  },
+};
